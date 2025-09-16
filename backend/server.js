e151b614017ab3cf37e6e4ea177e84053482c35d
doteur <zlatheomovich@gmail.com>
2025-09-16 11:36:02 +0200

    last changes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-console.log("hello world from node.js");
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+    });
